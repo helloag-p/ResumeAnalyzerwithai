cca1a8b262de981eeb72901e9620f744f299b569
helloag-p <parvagarwal04@gmail.com>
2025-09-15 18:18:50 +0530

    File Uploader done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,12 @@
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
-import {resumes}  from "../../constants/index";
+import { resumes } from "../../constants/index";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
@@ -11,22 +14,28 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate=useNavigate();
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-   <Navbar/>
+    <Navbar />
     <section className="main-section">
       <div className="page-heading">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
     </section>
 
 
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {resumes.map((resume)=>(
-          <ResumeCard key={resume.id} resume={resume}/>
-        ))}
-        </div>
-      )}
+
   </main>;
 }
